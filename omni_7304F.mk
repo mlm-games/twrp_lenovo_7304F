2655e015fedfeb2a14bcb923d6021677a4a7036e
= <=>
2024-08-17 12:35:27 +0530

    update for pbrp

--- omni_7304F.mk
+++ omni_7304F.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Causes the fast bootloop problem
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common pbrp stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from 7304F device
 $(call inherit-product, device/lenovo/7304F/device.mk)
