df3b7c53eb87c85aeeecfb446f5c0e227996e49c
= mlm-games <= gerrit.r6a6a@passinbox.com>
2024-09-08 10:03:14 +0530

    make some changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
-# 
+#
 
 # It is not system-as-root
 # lun path - /sys/bus/i2c/devices/i2c-0/device/driver/1100b000.i2c/subsystem/devices/soc/1000f000.pwrap/1000f000.pwrap:mt6392/mt6392-regulator/regulator/regulator.4/subsystem/regulator.14/musb-hdrc.0.auto-vusb/gadget/lun0/file
@@ -29,15 +29,15 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := generic
 
+# # Add back if it doesnt work
+# TARGET_IS_64_BIT := true 
+# TW_INCLUDE_FUSE_EXFAT := true  
+# TARGET_BOARD_SUFFIX := _64                    # Remove if the device is 32-bit
+# TARGET_USES_64_BIT_BINDER := true             # Remove if the device is 32-bit
 
-TARGET_IS_64_BIT := true 
-TW_INCLUDE_FUSE_EXFAT := true  
-TARGET_BOARD_SUFFIX := _64                    # Remove if the device is 32-bit
-TARGET_USES_64_BIT_BINDER := true             # Remove if the device is 32-bit
-
-# These two are for MTK Chipsets only
-BOARD_USES_MTK_HARDWARE := true
-BOARD_HAS_MTK_HARDWARE := true
+# # These two are for MTK Chipsets only
+# BOARD_USES_MTK_HARDWARE := true
+# BOARD_HAS_MTK_HARDWARE := true
 
 # APEX
 OVERRIDE_TARGET_FLATTEN_APEX := true
@@ -59,21 +59,19 @@ BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 # BOARD_KERNEL_IMAGE_NAME := Image
 # TARGET_KERNEL_CONFIG := 7304F_defconfig
 # TARGET_KERNEL_SOURCE := kernel/lenovo/7304F
-TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
-TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
-TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
+# TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
+# TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
+# TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
 
 # Kernel - prebuilt
-# ARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_FORCE_PREBUILT_KERNEL := true
 # ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 # endif
 
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
-
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_BOOTIMAGE_PARTITION_SIZE := 11088784
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216  #The img size is 11178896
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
@@ -83,8 +81,8 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Platform
 TARGET_BOARD_PLATFORM := mt8167
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME :=      # ro.product.board doesnt have a value...
+# TARGET_NO_BOOTLOADER := true
+# TARGET_BOOTLOADER_BOARD_NAME :=      # ro.product.board doesnt have a value...
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -100,71 +98,74 @@ PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
+# TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/bus/i2c/devices/i2c-0/device/driver/1100b000.i2c/subsystem/devices/soc/1000f000.pwrap/1000f000.pwrap:mt6392/mt6392-regulator/regulator/regulator.4/subsystem/regulator.14/musb-hdrc.0.auto-vusb/gadget/lun0/file
+# Add the below only if needed
 
-# #Save space
-# TW_EXCLUDE_ENCRYPTED_BACKUPS := true
-# TW_EXCLUDE_APP_MANAGER := true
-# # BOARD_HAS_NO_REAL_SDCARD := true # disables things like sdcard partitioning and may save you some space if TWRP isn't fitting in your recovery partition (for system-as-root?)
-# #TW_NO_EXFAT_FUSE := true
-# TARGET_BOOTANIMATION_TEXTURE_CACHE := false
-# TARGET_BOOTANIMATION_USE_RGB565 := true
-# TW_NO_SCREEN_TIMEOUT := true
-# TW_EXTRA_LANGUAGES := false
-# # TW_NO_FLASH_SPARK := true
-# # # To remove later
-# # TW_EXCLUDE_EXFAT := true
-# # TW_EXCLUDE_NTFS := true
-# # TW_NO_SDEXT_PARTITION := true
-# TW_NO_SCREEN_BLANK := true
-# TW_NO_USB_STORAGE := true
-# TW_EXCLUDE_EMOJI := true
-# TW_USE_COMPRESSION := false
-# # TW_EXCLUDE_MTP := true
+# TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/bus/i2c/devices/i2c-0/device/driver/1100b000.i2c/subsystem/devices/soc/1000f000.pwrap/1000f000.pwrap:mt6392/mt6392-regulator/regulator/regulator.4/subsystem/regulator.14/musb-hdrc.0.auto-vusb/gadget/lun0/file
+
+# # #Save space
+# # TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+# # TW_EXCLUDE_APP_MANAGER := true
+# # # BOARD_HAS_NO_REAL_SDCARD := true # disables things like sdcard partitioning and may save you some space if TWRP isn't fitting in your recovery partition (for system-as-root?)
+# # #TW_NO_EXFAT_FUSE := true
+# # TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+# # TARGET_BOOTANIMATION_USE_RGB565 := true
+# # TW_NO_SCREEN_TIMEOUT := true
+# # TW_EXTRA_LANGUAGES := false
+# # # TW_NO_FLASH_SPARK := true
+# # # # To remove later
+# # # TW_EXCLUDE_EXFAT := true
+# # # TW_EXCLUDE_NTFS := true
+# # # TW_NO_SDEXT_PARTITION := true
+# # TW_NO_SCREEN_BLANK := true
+# # TW_NO_USB_STORAGE := true
+# # TW_EXCLUDE_EMOJI := true
+# # TW_USE_COMPRESSION := false
+# # # TW_EXCLUDE_MTP := true
+# # TW_EXCLUDE_SUPERSU := true
+# # TW_NO_BATT_PERCENT := true
+# # # TW_INCLUDE_DUMLOCK := true #?
+# # TW_NO_CPU_TEMP := true
+# # TW_NO_HAPTICS := true
+# # TW_USE_MINUI := true
+# # TW_EXCLUDE_LPTOOLS := true
+# # TW_EXCLUDE_LPDUMP := true
+# # TW_EXCLUDE_NANO := true
+# # TW_EXCLUDE_BASH := true
+# # TW_EXCLUDE_PYTHON := true
+# # TW_EXCLUDE_TZDATA := true
+# # TW_EXCLUDE_TWRPAPP := true
+# # # TW_DISABLE_ADB := true
+# # # TW_DISABLE_FASTBOOT := true
+# # # TW_EXCLUDE_FILEMANAGER := true
+# # TW_EXCLUDE_TERMINAL := true
+# # # TW_EXCLUDE_PARTITIONMANAGER := true
+# # # LOCAL_STRIP_MODULE := true
+
+# # # Encryption remove (default?)
+# # TW_INCLUDE_CRYPTO := false
+# # TW_EXCLUDE_CRYPTO := true
+# # TW_EXCLUDE_FBE := true
+
+
+# # Properties
+# TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# # Trial
+
+# TARGET_USERIMAGES_USE_EXT4 := true
+# # BOARD_RECOVERYIMAGE_PARTITION_SIZE := 14088784
+# BOARD_FLASH_BLOCK_SIZE := 0
+# BOARD_HAS_NO_REAL_SDCARD := true
+# BOARD_HAS_NO_SELECT_BUTTON := true
+# BOARD_SUPPRESS_SECURE_ERASE := true
+# # BOARD_HAS_NO_MISC_PARTITION := true
+# BOARD_RECOVERY_SWIPE := true
+# BOARD_USES_MMCUTILS := true
+# BOARD_SUPPRESS_EMMC_WIPE := true
 # TW_EXCLUDE_SUPERSU := true
-# TW_NO_BATT_PERCENT := true
-# # TW_INCLUDE_DUMLOCK := true #?
-# TW_NO_CPU_TEMP := true
-# TW_NO_HAPTICS := true
-# TW_USE_MINUI := true
-# TW_EXCLUDE_LPTOOLS := true
-# TW_EXCLUDE_LPDUMP := true
-# TW_EXCLUDE_NANO := true
-# TW_EXCLUDE_BASH := true
-# TW_EXCLUDE_PYTHON := true
-# TW_EXCLUDE_TZDATA := true
-# TW_EXCLUDE_TWRPAPP := true
-# # TW_DISABLE_ADB := true
-# # TW_DISABLE_FASTBOOT := true
-# # TW_EXCLUDE_FILEMANAGER := true
-# TW_EXCLUDE_TERMINAL := true
-# # TW_EXCLUDE_PARTITIONMANAGER := true
-# # LOCAL_STRIP_MODULE := true
-
-# # Encryption remove (default?)
-# TW_INCLUDE_CRYPTO := false
-# TW_EXCLUDE_CRYPTO := true
-# TW_EXCLUDE_FBE := true
-
-
-# Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-
-# Trial
-
-TARGET_USERIMAGES_USE_EXT4 := true
-# BOARD_RECOVERYIMAGE_PARTITION_SIZE := 14088784
-BOARD_FLASH_BLOCK_SIZE := 0
-BOARD_HAS_NO_REAL_SDCARD := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-# BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_RECOVERY_SWIPE := true
-BOARD_USES_MMCUTILS := true
-BOARD_SUPPRESS_EMMC_WIPE := true
-TW_EXCLUDE_SUPERSU := true
-TW_INCLUDE_CRYPTO := true
+# TW_INCLUDE_CRYPTO := true
--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-# Android device tree for Lenovo Lenovo TB-7304F (TB-7304F)
-
-TWRP for Lenovo TB_7304F, currently does not work...
-
-For the very helpful links on how to unlock bootloader & rooting, check out [baejeogwan's readme](https://github.com/baejeongwan/twrp_device_lenovo_tb7304f)
-
-
-Currently I am stuck so will just be asking around xda or giithub if anyone else can help me fix it... It does boot forever sometimes and that kinda feels right as it doesnt bootloop and is only stuck on lenovo bootimage but i coould be wrong (kernel doesn't panic -> no bootloop right?)
-
-
-### Noted while building (for compiling)
-
-> Deleting multi_init causes it to reboot to stock rom
-
-> Easier way to reboot after flashing recovery.img and the device bootlooping: Hold vol- and power till the device reboots faster than usual, and then immediately only hold the vol+ button. Now you can flash your stock recovery or boot normally
+# TWRP device tree for Lenovo TB-7304F
